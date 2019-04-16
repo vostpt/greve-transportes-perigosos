bf646b36220c2380071fcf0ffea67c7afe47fb75
tomahock <eu@tomahock.com>
2019-04-16 17:19:18 +0100

    remove console log, better chart

--- js/main.js
+++ js/main.js
@@ -1,11 +1,8 @@
 $(function() {
     $("#dtBasicExample").tablesorter();
 
-    console.log($("#js-search"));
-    console.log($(".js-table tbody tr"));
     $("#js-search").keyup(function(){
         _this = this;
-        console.log(_this);
         $.each($(".js-table tbody tr"), function() {
             if($(this).text().toLowerCase().indexOf($(_this).val().toLowerCase()) === -1)
                 $(this).hide();
