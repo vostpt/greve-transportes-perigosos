3783b442c34da40e4a3469f688b498200258cd96
tomahock <eu@tomahock.com>
2019-04-16 21:44:54 +0100

    cache buster

--- stats.php
+++ stats.php
@@ -47,7 +47,7 @@ $antiXss = new AntiXSS();
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.19/css/dataTables.bootstrap.min.css"
           integrity="sha256-PbaYLBab86/uCEz3diunGMEYvjah3uDFIiID+jAtIfw=" crossorigin="anonymous"/>
     <link rel="stylesheet" href="css/bootstrap-theme.min.css">
-    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/main.css?cachebuster=lel2">
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js" integrity="sha256-0rguYS0qgS6L4qVzANq4kjxPLtvnp5nn2nB5G1lWRv4="
             crossorigin="anonymous"></script>
@@ -172,7 +172,7 @@ $antiXss = new AntiXSS();
 <script src="https://cdnjs.cloudflare.com/ajax/libs/livestamp/1.1.2/livestamp.min.js" integrity="sha256-8r65KJgULBDiZhwDydfWrEkx3yyV/grGsGzaekobngI="
         crossorigin="anonymous"></script>
 <script src="//cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
-<script src="js/main.js?cachebuster=lel"></script>
+<script src="js/main.js?cachebuster=lel2"></script>
 
 <script>
     $(document).ready(function () {
