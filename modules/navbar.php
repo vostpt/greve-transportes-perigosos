c28ef9ba82756b45e627e5df21e4bb5c94b1f1f8
tomahock <eu@tomahock.com>
2019-04-17 17:16:59 +0100

    fix all links

--- modules/navbar.php
+++ modules/navbar.php
@@ -13,17 +13,17 @@
 
 		<nav class="collapse navbar-collapse" id="bs-navbar">
 			<ul class="nav navbar-nav">
-				<li><a href="index.php">Início</a></li>
-				<li><a href="info.php">Informação Importante</a></li>
-				<li><a href="map/index.php">Mapa</a></li>
+				<li><a href="/index.php">Início</a></li>
+				<li><a href="/info.php">Informação Importante</a></li>
+				<li><a href="/map/index.php">Mapa</a></li>
 				<li class="nav-item dropdown">
 					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 						Estatísticas
 					</a>
 					<ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-						<li><a class="dropdown-item" href="stats.php">Estatísticas Gerais</a></li>
-						<li><a class="dropdown-item" href="by_county.php">Postos por Concelho</a></li>
-						<li><a class="dropdown-item" href="by_gas_type.php">Postos Gasolina vs Gasóleo</a></li>
+						<li><a class="dropdown-item" href="/stats.php">Estatísticas Gerais</a></li>
+						<li><a class="dropdown-item" href="/by_county.php">Postos por Concelho</a></li>
+						<li><a class="dropdown-item" href="/by_gas_type.php">Postos Gasolina vs Gasóleo</a></li>
 					</ul>
 				</li>
 			</ul>
