94b06e050b42bb60509b10a84a47f38de8cea310
tomahock <eu@tomahock.com>
2019-04-17 15:03:50 +0100

    bump cache buster css

--- stats.php
+++ stats.php
@@ -153,7 +153,7 @@ $antiXss = new AntiXSS();
 <script src="https://cdnjs.cloudflare.com/ajax/libs/livestamp/1.1.2/livestamp.min.js" integrity="sha256-8r65KJgULBDiZhwDydfWrEkx3yyV/grGsGzaekobngI="
         crossorigin="anonymous"></script>
 <script src="//cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
-<script src="js/main.js?cachebuster=lel2"></script>
+<script src="js/main.js?cachebuster=lel3"></script>
 
 <script>
     $(document).ready(function () {
