a3706f7a6efa735d851c47cf22c2977fe721a820
Pedro Pipoca <2161177@my.ipleiria.pt>
2019-04-17 15:31:13 +0100

    Fixed burger menu
    Fix: burger menu id

--- modules/navbar.php
+++ modules/navbar.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
 	<div class="container">
 		<div class="navbar-header">
-			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-navbar"
 					aria-expanded="false" aria-controls="navbar">
 				<span class="sr-only">Toggle navigation</span>
 				<span class="icon-bar"></span>
