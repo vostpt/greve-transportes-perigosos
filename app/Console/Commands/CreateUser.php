bc6897d6d8377a9673aa76ec87d34944890070df
Miguel Santos <github@miguelsantos.tech>
2019-04-18 14:25:45 +0100

    feat(Users): added artisan command to add users

---
+++ app/Console/Commands/CreateUser.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\User;
+use Illuminate\Support\Facades\Hash;
+
+class CreateUser extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'user:create {name} {email} {password}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new user';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        //
+        $user = new User();
+        $user->name = $this->argument('name');
+        $user->password = Hash::make($this->argument('password'));
+        $user->email = $this->argument('email');
+        $user->save();
+    }
+}
