ac635021b23d56fe655820c41fa79f8a47befab7
Miguel Santos <github@miguelsantos.tech>
2019-08-01 20:48:30 +0100

    fix(Mix): Css loading

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,7 +13,7 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css')
-   .css('resources/css/map.css', 'public/css')
+   .styles('resources/css/map.css', 'public/css')
    .js('resources/js/recaptcha.js', 'public/js')
    .js('resources/js/map.js', 'public/js')
    .js('resources/js/stats.js', 'public/js');
