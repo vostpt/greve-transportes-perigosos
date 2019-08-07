a60a3432f19ac97727ec45c89c56b9922ada3875
Miguel Santos <github@miguelsantos.tech>
2019-08-07 17:57:36 +0100

    fix(Stats): Iframe issues

--- resources/js/stats.js
+++ resources/js/stats.js
@@ -1,5 +1,8 @@
 function updateStats() {
     document.getElementById('global_stats').src = "/graphs/stats";
+    setTimeout(function() {
+        resizeIframe(document.getElementById('global_stats'));
+    }, 500);
     $.getJSON( "/storage/data/stats.json", (data) => {
         $("#entries_last_hour").html(data["entries_last_hour"]);
         $("#entries_last_day").html(data["entries_last_day"]);
