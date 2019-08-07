7565d48371d9789fe0315a2ba3e50bdf09ead3ce
Miguel Santos <github@miguelsantos.tech>
2019-08-07 10:53:35 +0100

    fix(CSRFToken): fixing issues

--- routes/web.php
+++ routes/web.php
@@ -31,56 +31,65 @@ Route::get('/media', function () {
 
 
 Auth::routes(['register' => false, 'verify' => true]);
+Route::get('panel/login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('panel/login', 'Auth\LoginController@login');
+Route::post('panel/logout', 'Auth\LoginController@logout')->name('logout');
+Route::get('panel/password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('panel/password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('panel/password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('panel/password/reset', 'Auth\ResetPasswordController@reset');
 
-Route::get('/home', 'HomeController@index')->name('home');
 
-Route::prefix('users')->name('users.')->middleware('auth')->group(function () {
-    Route::get('add', 'UsersController@add')->name('add');
-    Route::post('create', 'UsersController@create')->name('create');
-    Route::get('list', 'UsersController@list')->name('list');
-    Route::prefix('fetch')->name('fetch.')->group(function () {
-        Route::get('verified', 'UsersController@fetch_verified')->name('verified');
-        Route::get('not_verified', 'UsersController@fetch_not_verified')->name('not_verified');
+Route::prefix('panel')->group(function () {
+    Route::get('/home', 'HomeController@index')->name('home');
+    Route::prefix('users')->name('users.')->middleware('auth')->group(function () {
+        Route::get('add', 'UsersController@add')->name('add');
+        Route::post('create', 'UsersController@create')->name('create');
+        Route::get('list', 'UsersController@list')->name('list');
+        Route::prefix('fetch')->name('fetch.')->group(function () {
+            Route::get('verified', 'UsersController@fetch_verified')->name('verified');
+            Route::get('not_verified', 'UsersController@fetch_not_verified')->name('not_verified');
+        });
+        Route::get('password', 'UsersController@password')->name('password');
+        Route::post('verify', 'UsersController@verify')->name('verify');
+        Route::post('delete', 'UsersController@delete')->name('delete');
+        Route::post('updatePassword', 'UsersController@updatePassword')->name('updatePassword');
     });
-    Route::get('password', 'UsersController@password')->name('password');
-    Route::post('verify', 'UsersController@verify')->name('verify');
-    Route::post('delete', 'UsersController@delete')->name('delete');
-    Route::post('updatePassword', 'UsersController@updatePassword')->name('updatePassword');
-});
 
-Route::prefix('entries')->name('entries.')->group(function () {
-    Route::get('list', 'EntriesController@list')->middleware('auth')->name('list');
-    Route::prefix('fetch')->name('fetch.')->middleware('auth')->group(function () {
-        Route::get('pending', 'EntriesController@fetch_pending')->name('pending');
+    Route::prefix('entries')->name('entries.')->group(function () {
+        Route::get('list', 'EntriesController@list')->middleware('auth')->name('list');
+        Route::prefix('fetch')->name('fetch.')->middleware('auth')->group(function () {
+            Route::get('pending', 'EntriesController@fetch_pending')->name('pending');
+        });
+        Route::post('push', 'EntriesController@push')->middleware('auth')->name('push');
+        Route::post('add', 'EntriesController@add')->name('add');
     });
-    Route::post('push', 'EntriesController@push')->middleware('auth')->name('push');
-    Route::post('add', 'EntriesController@add')->name('add');
-});
 
-Route::prefix('stations')->name('stations.')->middleware('auth')->group(function () {
-    Route::get('list', 'FuelStationsController@list')->name('list');
-    Route::prefix('fetch')->name('fetch.')->group(function () {
-        Route::get('pending', 'FuelStationsController@fetch_all')->name('all');
+    Route::prefix('stations')->name('stations.')->middleware('auth')->group(function () {
+        Route::get('list', 'FuelStationsController@list')->name('list');
+        Route::prefix('fetch')->name('fetch.')->group(function () {
+            Route::get('pending', 'FuelStationsController@fetch_all')->name('all');
+        });
+        Route::post('update', 'FuelStationsController@update')->name('update');
     });
-    Route::post('update', 'FuelStationsController@update')->name('update');
-});
 
-Route::prefix('options')->name('options.')->middleware('auth')->group(function () {
-    Route::get('list', 'OptionsController@list')->name('list');
-    Route::prefix('fetch')->name('fetch.')->group(function () {
-        Route::get('all', 'OptionsController@fetch_all')->name('all');
+    Route::prefix('options')->name('options.')->middleware('auth')->group(function () {
+        Route::get('list', 'OptionsController@list')->name('list');
+        Route::prefix('fetch')->name('fetch.')->group(function () {
+            Route::get('all', 'OptionsController@fetch_all')->name('all');
+        });
+        Route::post('update', 'OptionsController@update')->name('update');
     });
-    Route::post('update', 'OptionsController@update')->name('update');
-});
 
-Route::prefix('externalauth')->name('externalauth.')->middleware('auth')->group(function () {
-    Route::get('add', 'ExternalAuthController@add')->name('add');
-    Route::post('create', 'ExternalAuthController@create')->name('create');
-    Route::get('list', 'ExternalAuthController@list')->name('list');
-    Route::prefix('fetch')->name('fetch.')->group(function () {
-        Route::get('all', 'ExternalAuthController@fetch_all')->name('all');
+    Route::prefix('externalauth')->name('externalauth.')->middleware('auth')->group(function () {
+        Route::get('add', 'ExternalAuthController@add')->name('add');
+        Route::post('create', 'ExternalAuthController@create')->name('create');
+        Route::get('list', 'ExternalAuthController@list')->name('list');
+        Route::prefix('fetch')->name('fetch.')->group(function () {
+            Route::get('all', 'ExternalAuthController@fetch_all')->name('all');
+        });
+        Route::post('delete', 'ExternalAuthController@delete')->name('delete');
     });
-    Route::post('delete', 'ExternalAuthController@delete')->name('delete');
 });
 
 Route::prefix('api/v1')->name('api.')->group(function () {
