fd5e8486b987405dbdea12699963d2fe2ca93153
Claudio Varandas <cvarandas@gmail.com>
2019-08-09 15:20:58 +0100

    feat(stats): dashboard first working version

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,7 +13,7 @@
     </div>
     <div class="row">
         <div class="col-md-4">
-            <div class="card" id="entries-card-total">
+            <div class="card">
                 <div class="card-content">
                     <div class="card-body">
                         <div class="media d-flex">
@@ -21,7 +21,7 @@
                                 <i class="fa fa-pencil-alt fa-3x float-left"></i>
                             </div>
                             <div class="media-body text-right">
-                                <h3>0</h3>
+                                <h3 id="entries-card-total">0</h3>
                                 <span>Total Submissões</span>
                             </div>
                         </div>
@@ -30,7 +30,7 @@
             </div>
         </div>
         <div class="col-md-4">
-            <div class="card" id="entries-card-hour">
+            <div class="card">
                 <div class="card-content">
                     <div class="card-body">
                         <div class="media d-flex">
@@ -38,7 +38,7 @@
                                 <i class="fa fa-pencil-alt fa-3x float-left"></i>
                             </div>
                             <div class="media-body text-right">
-                                <h3>0</h3>
+                                <h3 id="entries-card-hour">0</h3>
                                 <span>Subsmissões Ultima hora</span>
                             </div>
                         </div>
@@ -47,7 +47,7 @@
             </div>
         </div>
         <div class="col-md-4">
-            <div class="card" id="entries-card-day">
+            <div class="card">
                 <div class="card-content">
                     <div class="card-body">
                         <div class="media d-flex">
@@ -55,7 +55,7 @@
                                 <i class="fa fa-pencil-alt fa-3x float-left"></i>
                             </div>
                             <div class="media-body text-right">
-                                <h3>0</h3>
+                                <h3 id="entries-card-day">0</h3>
                                 <span>Subsmissões Ultimo Dia</span>
                             </div>
                         </div>
@@ -111,66 +111,131 @@
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script type="text/javascript">
 
-        $.getJSON( "/storage/data/stats_entries.json").then((data) => {
-            console.log(data);
-            let $cardTotal = document.getElementById('entries-card-total');
-            $($cardTotal).find('h3').text(data.entries_total);
-            let $cardDay = document.getElementById('entries-card-day');
-            $($cardDay).find('h3').text(data.entries_last_day);
-            let $cardHour = document.getElementById('entries-card-hour');
-            $($cardHour).find('h3').text(data.entries_last_hour);
-        });
+        let dataSourceGlobalUri = "/storage/data/stats_global.json";
+        let $countyEl = $("#county_selection");
+        let $districtEl = $("#district_selection");
 
-        $.getJSON( "/storage/data/stats_global.json").then((data) => {
+        window.onload = function(){
+            let district = sessionStorage.getItem('district');
+            let county = sessionStorage.getItem('county');
+            let dataSourceUri = sessionStorage.getItem('data_source_uri');
+            if(dataSourceUri === null && district === null && county === null){
+                $districtEl.val('none').trigger('change');
+                dataSourceUri = dataSourceGlobalUri;
+            }else{
+                $districtEl.val(district).trigger('change');
+                $countyEl.val(county).trigger('change');
+            }
+            sessionStorage.setItem('data_source_uri', dataSourceUri);
+            getEntries();
+            charts(dataSourceUri);
+        };
 
-            google.charts.load("current", {packages:["corechart"]});
-            google.charts.setOnLoadCallback(() => {
-                let dataTable1 = new google.visualization.DataTable();
+        setInterval(function () {
+            getEntries();
+            let ds = sessionStorage.getItem('data_source_uri');
+            charts(ds);
+        }, 30000);
 
-                dataTable1.addColumn('string', 'Combustivel');
-                dataTable1.addColumn('number', 'Postos');
-                dataTable1.addRows([
-                    ['Todos', data.stations_all ],
-                    ['Parte', data.stations_partial ],
-                    ['Nenhum', data.stations_none ]
-                ]);
-
-                let options = {
-                    pieHole: 0.2,
-                    chartArea: {
-                        top: 50,
-                        height: "300px"
-                    },
-                    height: 300,
-                    legend : {
-                        position: "top",
-                        alignment: "center",
-                    },
-                    pieSliceText: 'value-and-percentage',
-                    tooltip: {
-                        ignoreBounds:true
-                    },
-                    sliceVisibilityThreshold: 0
-                };
-
-                let optionsChart1 = Object.assign(options,{colors:['#8BC34A', '#f6bd00', '#f62317']});
-                let chart1 = new google.visualization.PieChart(document.getElementById('chart_container_1'));
-                chart1.draw(dataTable1, optionsChart1);
-
-                var dataTable2 = google.visualization.arrayToDataTable([
-                    ['Combustivel','Esgotado',{ role: 'annotation'},'Vende',{ role: 'annotation'}],
-                    ['Gasolina', data.stations_no_gasoline,data.stations_no_gasoline,data.stations_sell_gasoline,data.stations_sell_gasoline],
-                    ['Gasoleo', data.stations_no_diesel,data.stations_no_diesel,data.stations_sell_diesel,data.stations_sell_diesel],
-                    ['GPL', data.stations_no_lpg,data.stations_no_lpg,data.stations_sell_lpg,data.stations_sell_lpg]
-                ]);
-
-                let optionsChart2 = Object.assign(options,{legend: { position: 'top', maxLines: 3 },
-                    bar: { groupWidth: '75%' },
-                    isStacked: true,colors: ['#f62317','#8BC34A']});
-                let chart2 = new google.visualization.BarChart(document.getElementById('chart_container_2'));
-                chart2.draw(dataTable2, optionsChart2);
+        $.getJSON( "/storage/data/places.json", (data) => {
+            places = data;
+            Object.keys(data).forEach(district => {
+                $districtEl.append("<option value=\""+district+"\">"+district+"</option>")
             });
         });
+
+        $districtEl.on('change', function (e) {
+            let valueSelected = this.value;
+            if(valueSelected === "none") {
+                $countyEl.prop('disabled', true);
+                $countyEl.html("<option value=\"none\">Todos</option>");
+                sessionStorage.setItem('data_source_uri', dataSourceGlobalUri);
+                charts(dataSourceGlobalUri);
+            }else {
+                let dataSourceUri = "/storage/data/stats_" + encodeURI(valueSelected) + ".json";
+                $countyEl.prop('disabled', false);
+                $countyEl.html("<option value=\"none\">Todos</option>");
+                places[valueSelected].forEach(county => {
+                    $("#county_selection").append("<option value=\""+county+"\">"+county+"</option>");
+                });
+                sessionStorage.setItem('district', valueSelected);
+                sessionStorage.setItem('data_source_uri', dataSourceUri);
+                charts(dataSourceUri);
+            }
+        });
+
+        $('#county_selection').on('change', function (e) {
+            let valueSelected = this.value;
+            let district = $districtEl.val();
+            let dataSourceUri = "/storage/data/stats_" + encodeURI(district) + ".json";
+            if(valueSelected !== "none") {
+                dataSourceUri = "/storage/data/stats_" + encodeURI(district) + "_" + encodeURI(valueSelected) + ".json"
+            }
+            sessionStorage.setItem('county', valueSelected);
+            sessionStorage.setItem('data_source_uri', dataSourceUri);
+            charts(dataSourceUri);
+        });
+
+        function getEntries(){
+            $.getJSON('/storage/data/stats_entries.json').then((data) => {
+                $('#entries-card-total').text(data.entries_total);
+                $('#entries-card-day').text(data.entries_last_day);
+                $('#entries-card-hour').text(data.entries_last_hour);
+            });
+        }
+
+        function charts(dataSourceUri){
+            $.getJSON(dataSourceUri).then((data) => {
+
+                google.charts.load("current", {packages:["corechart"]});
+                google.charts.setOnLoadCallback(() => {
+                    let dataTable1 = new google.visualization.DataTable();
+
+                    dataTable1.addColumn('string', 'Combustivel');
+                    dataTable1.addColumn('number', 'Postos');
+                    dataTable1.addRows([
+                        ['Todos', data.stations_all ],
+                        ['Parte', data.stations_partial ],
+                        ['Nenhum', data.stations_none ]
+                    ]);
+
+                    let options = {
+                        pieHole: 0.2,
+                        chartArea: {
+                            top: 50,
+                            height: "300px"
+                        },
+                        height: 300,
+                        legend : {
+                            position: "top",
+                            alignment: "center",
+                        },
+                        pieSliceText: 'value-and-percentage',
+                        tooltip: {
+                            ignoreBounds:true
+                        },
+                        sliceVisibilityThreshold: 0
+                    };
+
+                    let optionsChart1 = Object.assign(options,{colors:['#8BC34A', '#f6bd00', '#f62317']});
+                    let chart1 = new google.visualization.PieChart(document.getElementById('chart_container_1'));
+                    chart1.draw(dataTable1, optionsChart1);
+
+                    var dataTable2 = google.visualization.arrayToDataTable([
+                        ['Combustivel','Esgotado',{ role: 'annotation'},'Vende',{ role: 'annotation'}],
+                        ['Gasolina', data.stations_no_gasoline,data.stations_no_gasoline,data.stations_sell_gasoline,data.stations_sell_gasoline],
+                        ['Gasoleo', data.stations_no_diesel,data.stations_no_diesel,data.stations_sell_diesel,data.stations_sell_diesel],
+                        ['GPL', data.stations_no_lpg,data.stations_no_lpg,data.stations_sell_lpg,data.stations_sell_lpg]
+                    ]);
+
+                    let optionsChart2 = Object.assign(options,{legend: { position: 'top', maxLines: 3 },
+                        bar: { groupWidth: '75%' },
+                        isStacked: true,colors: ['#f62317','#8BC34A']});
+                    let chart2 = new google.visualization.BarChart(document.getElementById('chart_container_2'));
+                    chart2.draw(dataTable2, optionsChart2);
+                });
+            });
+        }
     </script>
 
 @endsection
