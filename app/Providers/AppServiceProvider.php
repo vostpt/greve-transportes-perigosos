fe5a9a38551562263f1156ed100926d8542af32e
Miguel Santos <github@miguelsantos.tech>
2019-08-09 13:15:53 +0100

    fix(Entry): Auto Validation requires 1 extra count

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use App\Entry;
+use App\Observers\EntryObserver;
+
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,5 +28,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        Entry::observe(EntryObserver::class);
     }
 }
