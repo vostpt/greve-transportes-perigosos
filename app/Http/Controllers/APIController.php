7898498c1a9c2a7af9cda1e1c4b7bc9c10672185
Miguel Santos <github@miguelsantos.tech>
2019-08-14 18:50:26 +0100

    feat(Stations): Add Management Brand

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -39,7 +39,7 @@ class APIController extends Controller
                 if ($ext_auth->brand == 'READONLY' || $ext_auth->brand == 'WRITEREAD') {
                     $stations = FuelStation::all();
                 } else {
-                    $stations = FuelStation::where('brand', '=', $ext_auth->brand)->get();
+                    $stations = FuelStation::where('brand', '=', $ext_auth->brand)->orWhere('brand_management', '=', $ext_auth->brand)->get();
                 }
                 $output = [];
                 foreach ($stations as $station) {
@@ -98,7 +98,7 @@ class APIController extends Controller
                 $fuel_station = FuelStation::where('id', '=', $request->input('id'));
                 if ($fuel_station->count() > 0) {
                     $fuel_station = $fuel_station->first();
-                    if (($fuel_station->brand == $ext_auth->brand) || ($ext_auth->brand == 'WRITEREAD') || ($fuel_station->id == 972 && $ext_auth->brand == 'Tfuel') || ($fuel_station->id == 1549 && $ext_auth->brand == 'Tfuel')) {
+                    if (($fuel_station->brand == $ext_auth->brand) || ($fuel_station->brand_management == $ext_auth->brand) || ($ext_auth->brand == 'WRITEREAD') || ($fuel_station->id == 972 && $ext_auth->brand == 'Tfuel') || ($fuel_station->id == 1549 && $ext_auth->brand == 'Tfuel')) {
                         $has_gasoline = \intval($request->input('has_gasoline'));
                         $has_diesel   = \intval($request->input('has_diesel'));
                         $has_lpg      = \intval($request->input('has_lpg'));
@@ -141,62 +141,66 @@ class APIController extends Controller
         if ($request->has('key') && $request->has('secret') && $request->has('name') && $request->has('brand') && $request->has('repa') && $request->has('sell_gasoline') && $request->has('sell_diesel') && $request->has('sell_lpg') && $request->has('county') && $request->has('district') && $request->has('lat') && $request->has('long')) {
             $ext_auth = ExternalAuth::where([['key', '=', $request->input('key')],['secret', '=', $request->input('secret')]]);
             if ($ext_auth->count() > 0) {
-                $ext_auth = $ext_auth->first();
-                if (($request->input('brand') == $ext_auth->brand) || ($ext_auth->brand == 'WRITEREAD')) {
-                    $sell_gasoline = \intval($request->input('sell_gasoline'));
-                    $sell_diesel   = \intval($request->input('sell_diesel'));
-                    $sell_lpg      = \intval($request->input('sell_lpg'));
-                    if (! \is_nan($sell_gasoline) && ! \is_nan($sell_diesel) && ! \is_nan($sell_lpg)) {
-                        if ($sell_gasoline > 1) {
-                            $sell_gasoline = 1;
-                        } elseif ($sell_gasoline < 0) {
-                            $sell_gasoline = 0;
-                        }
-                        if ($sell_diesel > 1) {
-                            $sell_diesel = 1;
-                        } elseif ($sell_diesel < 0) {
-                            $sell_diesel = 0;
-                        }
-                        if ($sell_lpg > 1) {
-                            $sell_lpg = 1;
-                        } elseif ($sell_lpg < 0) {
-                            $sell_lpg = 0;
-                        }
-                        $county = $request->input('county');
-                        if ($county == null) {
-                            $county = '';
-                        }
-                        $district = $request->input('district');
-                        if ($district == null) {
-                            $district = '';
-                        }
-                        $repa = $request->input('repa');
-                        if ($repa == null) {
-                            $repa = '';
-                        }
-                        $data = [
-                                'repa'          => $repa,
-                                'source_id'     => 'api',
-                                'brand'         => $request->input('brand'),
-                                'name'          => $request->input('name'),
-                                'sell_gasoline' => $sell_gasoline,
-                                'sell_diesel'   => $sell_diesel,
-                                'sell_lpg'      => $sell_lpg,
-                                'has_gasoline'  => $sell_gasoline,
-                                'has_diesel'    => $sell_diesel,
-                                'has_lpg'       => $sell_lpg,
-                                'long'          => \floatval($request->input('lat')),
-                                'lat'           => \floatval($request->input('long')),
-                                'county'        => $county,
-                                'district'      => $district,
-                            ];
-                        $fuel_station = new FuelStation();
-                        $fuel_station->fill($data);
-                        $fuel_station->save();
-                        $cacheController = new CacheController();
-                        $cacheController->updateStations();
-                        $output = ['success' => 1];
+                $ext_auth      = $ext_auth->first();
+                $sell_gasoline = \intval($request->input('sell_gasoline'));
+                $sell_diesel   = \intval($request->input('sell_diesel'));
+                $sell_lpg      = \intval($request->input('sell_lpg'));
+                if (! \is_nan($sell_gasoline) && ! \is_nan($sell_diesel) && ! \is_nan($sell_lpg)) {
+                    $brand            = $request->input('brand');
+                    $management_brand = '';
+                    if ($brand != $ext_auth->brand && $ext_auth->brand != 'WRITEREAD') {
+                        $management_brand = $ext_auth->brand;
                     }
+                    if ($sell_gasoline > 1) {
+                        $sell_gasoline = 1;
+                    } elseif ($sell_gasoline < 0) {
+                        $sell_gasoline = 0;
+                    }
+                    if ($sell_diesel > 1) {
+                        $sell_diesel = 1;
+                    } elseif ($sell_diesel < 0) {
+                        $sell_diesel = 0;
+                    }
+                    if ($sell_lpg > 1) {
+                        $sell_lpg = 1;
+                    } elseif ($sell_lpg < 0) {
+                        $sell_lpg = 0;
+                    }
+                    $county = $request->input('county');
+                    if ($county == null) {
+                        $county = '';
+                    }
+                    $district = $request->input('district');
+                    if ($district == null) {
+                        $district = '';
+                    }
+                    $repa = $request->input('repa');
+                    if ($repa == null) {
+                        $repa = '';
+                    }
+                    $data = [
+                                'repa'             => $repa,
+                                'source_id'        => 'api',
+                                'brand'            => $request->input('brand'),
+                                'brand_management' => $management_brand,
+                                'name'             => $request->input('name'),
+                                'sell_gasoline'    => $sell_gasoline,
+                                'sell_diesel'      => $sell_diesel,
+                                'sell_lpg'         => $sell_lpg,
+                                'has_gasoline'     => $sell_gasoline,
+                                'has_diesel'       => $sell_diesel,
+                                'has_lpg'          => $sell_lpg,
+                                'long'             => \floatval($request->input('lat')),
+                                'lat'              => \floatval($request->input('long')),
+                                'county'           => $county,
+                                'district'         => $district,
+                            ];
+                    $fuel_station = new FuelStation();
+                    $fuel_station->fill($data);
+                    $fuel_station->save();
+                    $cacheController = new CacheController();
+                    $cacheController->updateStations();
+                    $output = ['success' => 1];
                 }
             }
         }
@@ -213,7 +217,12 @@ class APIController extends Controller
                 $fuel_station = FuelStation::where('id', '=', $request->input('id'));
                 if ($fuel_station->count() > 0) {
                     $fuel_station = $fuel_station->first();
-                    if (($fuel_station->brand == $ext_auth->brand) || ($ext_auth->brand == 'WRITEREAD') || ($fuel_station->id == 972 && $ext_auth->brand == 'Tfuel') || ($fuel_station->id == 1549 && $ext_auth->brand == 'Tfuel')) {
+                    if (($fuel_station->brand == $ext_auth->brand) || ($fuel_station->brand_management == $ext_auth->brand) || ($ext_auth->brand == 'WRITEREAD') || ($fuel_station->id == 972 && $ext_auth->brand == 'Tfuel') || ($fuel_station->id == 1549 && $ext_auth->brand == 'Tfuel')) {
+                        $brand            = $request->input('brand');
+                        $management_brand = '';
+                        if ($brand != $ext_auth->brand && $ext_auth->brand != 'WRITEREAD') {
+                            $management_brand = $ext_auth->brand;
+                        }
                         $sell_gasoline = \intval($request->input('sell_gasoline'));
                         $sell_diesel   = \intval($request->input('sell_diesel'));
                         $sell_lpg      = \intval($request->input('sell_lpg'));
@@ -246,20 +255,21 @@ class APIController extends Controller
                                 $repa = '';
                             }
                             $data = [
-                                'repa'          => $repa,
-                                'source_id'     => 'api',
-                                'brand'         => $request->input('brand'),
-                                'name'          => $request->input('name'),
-                                'sell_gasoline' => $sell_gasoline,
-                                'sell_diesel'   => $sell_diesel,
-                                'sell_lpg'      => $sell_lpg,
-                                'has_gasoline'  => $sell_gasoline,
-                                'has_diesel'    => $sell_diesel,
-                                'has_lpg'       => $sell_lpg,
-                                'long'          => \floatval($request->input('lat')),
-                                'lat'           => \floatval($request->input('long')),
-                                'county'        => $county,
-                                'district'      => $district,
+                                'repa'             => $repa,
+                                'source_id'        => 'api',
+                                'brand'            => $request->input('brand'),
+                                'brand_management' => $management_brand,
+                                'name'             => $request->input('name'),
+                                'sell_gasoline'    => $sell_gasoline,
+                                'sell_diesel'      => $sell_diesel,
+                                'sell_lpg'         => $sell_lpg,
+                                'has_gasoline'     => $sell_gasoline,
+                                'has_diesel'       => $sell_diesel,
+                                'has_lpg'          => $sell_lpg,
+                                'long'             => \floatval($request->input('lat')),
+                                'lat'              => \floatval($request->input('long')),
+                                'county'           => $county,
+                                'district'         => $district,
                             ];
                             $fuel_station->fill($data);
                             $fuel_station->save();
